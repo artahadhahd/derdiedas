4084695c1e5e742a83650ce585f8bc0d380e6a60
artahadhahd <artaazadegan1@gmail.com>
2024-06-16 17:58:10 +0200

    fixed the game and improved ux

--- App.tsx
+++ App.tsx
@@ -1,15 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import Home from './pages/Home';
 import Words from './pages/Words';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-
-// const Stack = createNativeStackNavigator();
 
 
 const Tab = createBottomTabNavigator();
@@ -38,31 +31,3 @@ export default function App() {
     </NavigationContainer>
   )
 }
-
-
-
-// export default function App() {
-//   return (
-//     // <View style={styles.container}>
-//     //   <Text>Open up App.tsx to start working on your app!</Text>
-//     //   <StatusBar style="auto" />
-//     // </View>
-//     <>
-//       <NavigationContainer>
-//         <Stack.Navigator>
-//           <Stack.Screen name="words" component={Words}/>
-//           <Stack.Screen name="Home" component={Home}/>
-//         </Stack.Navigator>
-//       </NavigationContainer>
-//     </>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
